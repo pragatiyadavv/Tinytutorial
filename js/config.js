82374aaba8f08a0a0a808b33385b20756d2bc4b9
Pragati Yadav <73021425+pragatiyadavv@users.noreply.github.com>
2026-02-05 14:27:03 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,18 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8Me7W1EQa', // changed
-        userPoolClientId: '2tt6ocddhdmpcigumf15elksda', // e.g. changes
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_8Me7W1EQa',
+        userPoolClientId: '2tt6ocddhdmpcigumf15elksda',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://ah93yq8y22.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''   // we will add later
     }
 };
-
-
-
-
-
-
-
-
